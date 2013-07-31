dcbc050bab536e88055b0e85d74082866929477b
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2013-07-31 12:00:36 +0000

    CSV-32 Parameterize BeanReader and BeanWriter classes

--- OSS/eu.ralph-schuster.csv/trunk/src/test/java/csv/impl/CSVWriterReaderTest.java
+++ OSS/eu.ralph-schuster.csv/trunk/src/test/java/csv/impl/CSVWriterReaderTest.java
@@ -30,8 +30,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import rs.baselib.util.CommonUtils;
 import csv.CommentCallback;
 import csv.TableReader;
+import csv.util.CSVUtils;
 
 /**
  * JUnit Test for CSV.
@@ -39,8 +41,12 @@ import csv.TableReader;
  *
  */
 public class CSVWriterReaderTest {
+	
 	private static final String FILE_NAME= "csv-test.csv";
-
+	
+	/** Character encoding for file operations */
+	private static final String DEFAULT_CHARSET = "utf8";
+	
 	private final static String COMMENT_VALUE= "ORIGINAL COMMENT";
 
 	private static final String TEST_HEADER[] = new String[] {
@@ -58,7 +64,8 @@ public class CSVWriterReaderTest {
 		{"\n3:0", "3:1\n2.Zeile", "3:2\n\n3.Zeile", "\n\n3:3\n4.Zeile"},
 		// encoding
 		{"š  ť  ž  ľ  č  ě ď ň ř ů ĺ Š Ť Ž Ľ Č Ě Ď Ň Ř Ů Ĺ", "ł ą ż ę ć ń ś ź Ł Ą Ż Ę Ć Ń Ś Ź", "Ă ă Ş ş Ţ ţ", " š č ž ć đ  Š Č Ž Ć Đ", " Ő ő Ű ű", "Ä, ä, Ö, ö, Ü, ü, ß",
-			"абвгдеёжзийклмно прстуфхцчшчьыъэюя АБВГДЕЁЖЗИЙ КЛМНОПРСТУФ ХЦЧШЩЬЫЪЭЮЯ", "Ў ў Є є Ґ ґ", "Ђ Љ Њ Ћ Џ ђ љ њ ћ џ"}};
+			"абвгдеёжзийклмно прстуфхцчшчьыъэюя АБВГДЕЁЖЗИЙ КЛМНОПРСТУФ ХЦЧШЩЬЫЪЭЮЯ", "Ў ў Є є Ґ ґ", "Ђ Љ Њ Ћ Џ ђ љ њ ћ џ"}
+	};
 
 	private File fFile;
 
@@ -68,6 +75,7 @@ public class CSVWriterReaderTest {
 	@Before
 	public void init() {
 		fFile= new File(FILE_NAME);
+		CSVUtils.setDefaultCharset(DEFAULT_CHARSET);
 	}
 
 	/**
@@ -94,10 +102,10 @@ public class CSVWriterReaderTest {
 	 * @param withHeader whether header shall be written.
 	 */
 	protected void testWriteFile(boolean withHeader) {
-		CSVWriter out= null;
+		CSVWriter out = null;
 		try {
 			// Write the file
-			out= new CSVWriter(fFile);
+			out = new CSVWriter(fFile);
 			if (withHeader) out.printRow(TEST_HEADER);
 			for (int row= 0; row < TEST_VALUES.length; row++) {
 				out.printRow(TEST_VALUES[row]);
@@ -123,15 +131,17 @@ public class CSVWriterReaderTest {
 			
 			// Test the header
 			if (withHeader) {
+				int lineNo = in.getLineCount();
 				Object header[] = in.getHeaderRow();
-				testRow(TEST_HEADER, header);
+				testRow(TEST_HEADER, header, lineNo);
 			}
 			
 			// Test the data
 			while (in.hasNext()) {
 				Object columns[]= in.next();
 
-				testRow(TEST_VALUES[row], columns);
+				int lineNo = in.getLineCount();
+				testRow(TEST_VALUES[row], columns, lineNo);
 				
 				row++;
 			}
@@ -151,17 +161,18 @@ public class CSVWriterReaderTest {
 	 * @param master master data array
 	 * @param copy copy data array to be compared
 	 */
-	protected void testRow(Object master[], Object copy[]) {
+	protected void testRow(Object master[], Object copy[], int lineNo) {
 		// compare size of columns
-		assertEquals(master.length, copy.length);
+		StringBuffer s = new StringBuffer(); CommonUtils.debugObject(s, copy);
+		assertEquals("Line "+lineNo+": array length check - "+s.toString(), master.length, copy.length);
 		for (int col=0; col<copy.length; col++) {
 			if ((master[col] != null)) {
-				assertNotNull(copy[col]);
+				assertNotNull("Line "+lineNo+", Column "+col+": null", copy[col]);
 			}
 			if ((copy[col] != null)) {
-				assertNotNull(master[col]);
+				assertNotNull("Line "+lineNo+", Column "+col+": null", master[col]);
 			}
-			assertEquals(master[col], copy[col]);
+			assertEquals("Line "+lineNo+", Column "+col+": not equal", master[col], copy[col]);
 		}
 	}
 	
@@ -181,9 +192,9 @@ public class CSVWriterReaderTest {
 	@Test
 	public void testCommentCallback() {
 
-		CSVWriter out= null;
+		CSVWriter out = null;
 		try {
-			out= new CSVWriter(fFile);
+			out = new CSVWriter(fFile);
 			for (int row= 0; row < TEST_VALUES.length; row++) {
 				out.printRow(TEST_VALUES[row]);
 			}
@@ -195,7 +206,7 @@ public class CSVWriterReaderTest {
 		}
 		CSVReader in= null;
 		try {
-			in= new CSVReader(fFile);
+			in = new CSVReader(fFile);
 			in.registerCommentCallBack(new TestCallback());
 			in.setIgnoreComments(false);
 			while (in.hasNext()) {
